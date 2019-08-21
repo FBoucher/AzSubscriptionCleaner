aebac5adfe462b41fd95d84cde0477a3879d5249
fboucher <fboucher@outlook.com>
2019-08-21 16:30:44 -0400

    Added function

---
+++ AzSubCleaner/AzSubCleanerTimeTrigger/run.ps1
@@ -0,0 +1,31 @@
+# Input bindings are passed in via param block.
+param($Timer)
+
+# Get the current universal time in the default string format
+$currentUTCtime = (Get-Date).ToUniversalTime()
+
+# The 'IsPastDue' porperty is 'true' when the current function invocation is later than scheduled.
+if ($Timer.IsPastDue) {
+    Write-Host "PowerShell timer is running late!"
+}
+
+# Write an information log with the current time.
+Write-Host "PowerShell timer trigger function ran! TIME: $currentUTCtime"
+
+$expResources= Search-AzGraph -Query 'where todatetime(tags.expireOn) < now() | project id'
+
+foreach ($r in $expResources) {
+    Write-Host "Expired Resource ID=" + $r.id
+    Remove-AzResource -ResourceId $r.id -Force -WhatIf
+}
+
+$rgs = Get-AzResourceGroup;
+ 
+foreach($resourceGroup in $rgs){
+    $name=  $resourceGroup.ResourceGroupName;
+    $count = (Get-AzResource | Where-Object{ $_.ResourceGroupName -match $name }).Count;
+    if($count -eq 0){
+        Write-Host "==> $name is empty. Deleting it...";
+        Remove-AzResourceGroup -Name $name -Force -WhatIf
+    }
+}
